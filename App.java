dcd6a3c6fcce9c945626381e9d337ddee97b1ab8
lukacseszter <lukacseszter.leo@gmail.com>
2022-02-22 08:53:00 +0100

    Fejlec es scanner

---
+++ App.java
@@ -0,0 +1,17 @@
+/*
+* File: App.java
+* Author: Lukács Eszter
+* Copyright: 2022, Lukács Eszter
+* Group: Szoft I N
+* Date: 2022-02-22
+* Github: https://github.com/lukacseszter/
+* Licenc: GNU GPL
+*/
+
+class App {
+public static void main (String[] args) {
+System.out.println("Lukacs Eszter, 2022-02-22");
+
+
+}	
+}
